954dc19638d155f98e4f651ae5b1a8d494f6bbd4
khactien <khactien9d@gmail.com>
2021-06-06 11:14:43 +0700

    product-api

--- Products_Api/src/App.js
+++ Products_Api/src/App.js
@@ -5,26 +5,68 @@ function App() {
     return (
         <div>
             <div className="navbar navbar-default">
-                <a href className="navbar-brand">CALL API</a>
+                <a href className="navbar-brand">TiếnNK API</a>
                 <ul className="nav navbar-nav">
-                <li >
-                    <a href >Trang Chủ</a>
-                </li>
-                <li>
-                    <a href>Quản lý sản phẩm</a>
-                </li>
+                    <li >
+                        <a href >Trang Chủ</a>
+                    </li>
+                    <li>
+                        <a href>Quản lý sản phẩm</a>
+                    </li>
                 </ul>
             </div>
 
         
-            <div class="container">
+            <div className="container">
                 
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        
+                <div className="row">
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <button type="button" className="btn btn-info md-10">
+                            Thêm sản phẩm
+                        </button>
                     </div>
                 </div>
-            
+                
+                <div className="panel panel-primary">
+                        <div className="panel-heading">
+                                <h3 className="panel-title">Danh sách sản phẩm</h3>
+                        </div>
+                        <div className="panel-body">
+                            
+                            <table className="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>STT</th>
+                                        <th>Mã</th>
+                                        <th>Tên</th>
+                                        <th>Giá</th>
+                                        <th>Trạng thái</th>
+                                        <th>Hành Động</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>1</td>
+                                        <td>Iphone 6 Plus</td>
+                                        <td>500</td>
+                                        <td>
+                                            <span class="label label-warning">Còn Hàn</span>
+                                        </td>
+                                        <td>
+                                            
+                                            <button type="button" class="btn btn-success mr-10">Sửa</button>
+                                            
+                                            <button type="button" class="btn btn-danger ">Xóa</button>
+                                            
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            
+                        </div>
+                </div>
+                
             </div>
         
         </div>
